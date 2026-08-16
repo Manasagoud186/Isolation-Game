bdde76625cb36cf58f4c9f315c0e7b8644118ddd
Manasa <gmanasa125@gmail.com>
2026-08-16 21:31:50 +0530

    Update Isolation Game documentation

--- manasa/main.java/.vscode/class StaticBinding {.java
+++
@@ -1,9 +0,0 @@
-class StaticBinding {
-    static void show() {
-        System.out.println("Static Binding");
-    }
-
-    public static void main(String[] args) {
-        show();
-    }
-}
--- manasa/samrt id card/frontend/src/components/layout/DashboardLayout.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <div className="p-8">{children}</div>;
-}
